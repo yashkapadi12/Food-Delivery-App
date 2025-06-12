35d60b2a01985dd8b891e615b418bc033268d592
Yash kapadi <68005118+yashkapadi12@users.noreply.github.com>
2025-06-12 16:52:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoDB = require("./db");
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoDB();
